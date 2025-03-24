7955e6e510cbf30d39db4b3ff39da910f02b4d8a
Илья <128465818+Karasik876@users.noreply.github.com>
2025-03-24 18:27:25 +0500

    fix: no migrations needed

--- entrypoint_tests.sh
+++ entrypoint_tests.sh
@@ -1,11 +1,2 @@
 #!/bin/sh
-echo "Waiting for database migrations to apply..."
-
-until uv run alembic upgrade head; do
-  echo "Waiting for migrations to apply..."
-  sleep 2
-done
-
-echo "Migrations applied. Running tests..."
-
 exec uv run pytest -v --durations=0 --cov --cov-report=xml "$@"
